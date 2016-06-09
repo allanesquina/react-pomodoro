ac89d0ea97aab21b842c75f319bc4e1c81c90608
allanesquina <loloudxur@gmail.com>
2016-06-09 03:18:24 -0300

    AlertBox component | removing unused files

---
+++ src/components/Pomodoro/AlertBox.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import classes from './AlertBox.scss'
+import ActionButton from './ActionButton'
+
+export const AlertBox = (props) => (
+  <div className={classes.box}>
+    <p>It's time to relax ;-)</p>
+    <ActionButton onClick={props.onClick} text='Close'/>
+  </div>
+)
+
+AlertBox.propTypes = {
+  onClick: React.PropTypes.func
+}
+
+export default AlertBox
